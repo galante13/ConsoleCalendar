e5e08a067c65e2ce42109cd80d96411f665106e2
Gustaw Wypych <gustaw.wypych@gmail.com>
2017-01-29 21:27:26 +0100

    Adding Menu and Node entity to Entities in Domain

---
+++ src/ConsoleCalendar/Naukowcy.ConsoleCalendar.Domain/Entities/Menu/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Naukowcy.ConsoleCalendar.Domain.Entities
+namespace Naukowcy.ConsoleCalendar.Domain.Entities
+{
+    public class Menu
+    {
+        public static Menu Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (SyncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Menu();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private Menu()
+        {
+        }
+
+        private static volatile Menu _instance;
+        private static readonly object SyncRoot = new object();
+
+        public ICollection<Node> Nodes { get; private set; }
+
+    }
+}
---
+++ src/ConsoleCalendar/Naukowcy.ConsoleCalendar.Domain/Entities/Menu/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Naukowcy.ConsoleCalendar.Domain.Entities
+{
+    public class Node
+    {
+        public string Message { get; private set; }
+        public NodeOption OptionId { get; private set; }
+        public Action OnOptionChosen { get; private set; }
+
+        public Node(
+            string message,
+            NodeOption optionId,
+            Action onOptionChosen)
+        {
+            Message = message;
+            OptionId = optionId;
+            OnOptionChosen = onOptionChosen;
+        }
+
+        public void Draw()
+        {
+            //TODO Implement
+        }
+    }
+
+    public enum NodeOption
+    {
+        ShowToday = 1,
+        ShowActualMonth = 2,
+        ShowGivenMonth = 3,
+        AddNote = 4,
+        ShowNotes = 5,
+        Close = 6
+    }
+}
